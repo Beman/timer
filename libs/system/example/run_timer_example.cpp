5acd903f205a5a8b356927d2f6f85a9502cf27ca
Beman <bdawes@acm.org>
2011-09-13 09:33:50 -0400

    Change docs to indicate upcoming changes

--- libs/system/example/run_timer_example.cpp
+++ libs/system/example/run_timer_example.cpp
@@ -2,10 +2,8 @@
 
 //  Copyright Beman Dawes 2006
 
-//  Distributed under the Boost Software License, Version 1.0. (See accompanying
-//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-
-//  See http://www.boost.org/libs/system for documentation.
+//  Distributed under the Boost Software License, Version 1.0.
+//  See http://www.boost.org/LICENSE_1_0.txt
 
 #include <boost/system/timer.hpp>
 #include <cmath>
