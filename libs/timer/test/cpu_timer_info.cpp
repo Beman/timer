779e24b1be034e764db23c55ada73859a3d7507b
Beman <bdawes@acm.org>
2011-09-30 12:07:59 -0400

    Add "steady" reporting.

--- libs/timer/test/cpu_timer_info.cpp
+++ libs/timer/test/cpu_timer_info.cpp
@@ -8,6 +8,7 @@
 //  See http://www.boost.org/libs/timer for documentation.
 
 #include <boost/timer/timer.hpp>
+#include <boost/chrono/chrono.hpp>
 #include <boost/detail/lightweight_main.hpp>
 #include <cstdlib> // for atol()
 #include <iostream>
@@ -17,18 +18,30 @@ using boost::timer::nanosecond_type;
 using boost::timer::cpu_times;
 using boost::timer::cpu_timer;
 using boost::timer::auto_cpu_timer;
-
+using std::cout; using std::endl;
 
 int cpp_main( int argc, char * argv[] )
 {
+  cout << '\n';
+  cout << "For cpu_times.wall, the underlying clock "
+       << (boost::chrono::high_resolution_clock::is_steady
+           ? "is steady"
+           : "is not steady"
+          )
+       << "Steady clocks are defined by C++11 as clocks for which values "
+          "of time_point never decrease as physical time advances and for "
+          "which values of time_point advance at a steady rate relative to "
+          "real time. That is, the clock may not be adjusted."
+       << endl;
+  
   cpu_times start_time;
   start_time.clear();
   cpu_times current_time;
 
   {
     cpu_timer cpu;
-    std::cout << "measure boost::timer::cpu_timer resolution for user time..."
-              << std::endl;
+    cout << "measure boost::timer::cpu_timer resolution for user time..."
+              << endl;
     for (int i = 0; i < 3; ++i)
     {
       cpu.start();
@@ -38,14 +51,14 @@ int cpp_main( int argc, char * argv[] )
       {
         current_time = cpu.elapsed();
       }
-      std::cout << current_time.user - start_time.user << "ns\n";
+      cout << current_time.user - start_time.user << "ns\n";
     }
   }
  
   {
     cpu_timer cpu;
-    std::cout << "measure boost::timer::cpu_timer resolution for wall-clock time..."
-              << std::endl;
+    cout << "measure boost::timer::cpu_timer resolution for wall-clock time..."
+              << endl;
     for (int i = 0; i < 100; ++i)
     {
       cpu.start();
@@ -55,7 +68,7 @@ int cpp_main( int argc, char * argv[] )
       {
         current_time.wall = cpu.elapsed().wall;
       }
-      std::cout << current_time.wall - start_time.wall << "ns ";
+      cout << current_time.wall - start_time.wall << "ns ";
     }
   }
  return 0;
